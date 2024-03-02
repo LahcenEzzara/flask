c8f46c5cd8fc146a0db012b806ce01d51889233e
Lahcen Ezzara <114707306+LahcenEzzara@users.noreply.github.com>
2024-03-02 23:48:55 +0100

    Update

---
+++ app.py
@@ -0,0 +1,13 @@
+# save this as app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
